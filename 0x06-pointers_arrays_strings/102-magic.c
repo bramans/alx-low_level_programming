aa80464671b1ee6763410e9d8aa20d9e511589e1
Ibrahim Mansour <mansourbrahim0@gmail.com>
2023-09-21 03:15:31 -0700

    MorePointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,18 +2,21 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
-  
-  // Use pointer arithmetic to indirectly modify a[2] via p
-  *(p + 7) = 98;
-
-  /* This should print a[2] = 98 */
-  printf("a[2] = %d\n", a[2]);
-  return 0;
+ 	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 
+	 */		
+	 *p = 98;
+	 /* ... prints 98\n */
+	 printf("a[2] = %d\n", a[2]);
+	 return (0);
 }
 
